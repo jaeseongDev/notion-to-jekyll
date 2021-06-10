a293d75440f09d9c426a31c4d357570b980492c1
jaeseongDev <qkrwotjd1445@naver.com>
2021-06-11 03:05:28 +0900

    feat : 파일 읽어들이는 기능 추가

---
+++ src/test/java/FileReaderTest.java
@@ -0,0 +1,33 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FileReaderTest {
+
+    @Test
+    @DisplayName("파일을 정상적으로 읽어들이는 지 체크")
+    public void read() throws IOException {
+        // given
+        String writingText = "hello\ntest";
+        byte[] writingBytes = writingText.getBytes();
+        Path path = Paths.get("./src/main/resources/test.txt");
+        Files.write(path, writingBytes);
+
+        // when
+        File file = new File(path.toString());
+        FileReader fileReader = new FileReader(path);
+        String readingText = fileReader.read();
+
+        // then
+        assertThat(file.exists()).isTrue();
+        assertThat(readingText).isEqualTo(writingText);
+        file.delete();
+    }
+}
